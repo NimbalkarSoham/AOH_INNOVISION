340f66bacb158526e2f19c377725b44f9e2bccb0
Darash Mishra <107783346+DM1ShRa@users.noreply.github.com>
2024-03-31 05:07:38 +0530

    Demand Forecasting

--- backend/server.py
+++ backend/server.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pickle
 from models.price_prediction import predict_price
 from flask_cors import CORS
+from models.tool_predict import predict_tools
 
 app = Flask(__name__)
 app = Flask(__name__, static_url_path='/static')
@@ -18,6 +19,10 @@ def get_predicted_price():
     data = request.json
     predicted_price = predict_price(data)
     return jsonify({'predicted_price': predicted_price})
+
+@app.route('/toolpredict', methods=['POST'])
+def predict():
+    return predict_tools()
 
 if __name__ == '__main__':
     app.run(debug=True)
